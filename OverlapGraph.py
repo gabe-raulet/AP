1cde4330a5d5679e2b4e30a5aa317a74f3ea6dfc
Gabriel Raulet <gabe.h.raulet@gmail.com>
2022-06-16 15:36:38 -0700

    for now

--- OverlapGraph.py
+++ OverlapGraph.py
@@ -4,15 +4,59 @@ class OverlapGraph(object):
     """
     @class OverlapGraph:
 
-        Class for an overlap graph G = (V,E) where V = \{0,...,self.n-1}
+        Class for a bidirected overlap graph G = (V,E) where V = \{0,...,self.n-1\}
         is the set of reads/vertices and E is the set of read overlaps.
+
+        Below are examples/descriptions of each kind of overlap and their associated
+        semantic representation in the graph.
+
+                        012*45678*
+        ############  u --------->         beg(u) = 3, end(u) = 9, len(u) = 9
+        # Regular  #       ||||||
+        # Dovetail #     v ---------->     beg(v) = 0, end(v) = 6, len(v) = 10
+        ############       *12345*789
+
+        In the above case, a suffix of u aligns to a prefix of v. This is called
+        a Regular Dovetail. In the overlap graph, we represent this with a directed
+        bidirected edge (u >--> v). We label this edge (semantically) with the overhanging
+        suffix of v which does not align with u, namely v[end(v)..).
+            Technically, this is all the information required for this scenario and this
+        is a valid bidirected edge. However, since we will be using the graph to perform
+        walks/traversals to build contigs, we can make the job easier right now to save
+        time later. Specifically, imagine that in our traversal we passed from v to u,
+        which is the opposite direction of the edge's orientation. In such a case we don't
+        care about the suffix of v; we need the prefix of u, and more specifically, the
+        reverse complement of the prefix of u. It is possible to calculate this from
+        the information given so far. Namely, if l(u >--> v) denotes the length of the
+        sequence labeling the edge (u >--> v), so that v[|v|-l..) is the label, then we
+        can calculate the length of the overhanging prefix of u to be m = |u| + l - |v|,
+        and then the prefix of interest is comp(u[..m)). However, it is easier to just label
+        this immediately. We accomplish this by creating a shadow edge for the prefix direction,
+        which can be looked up as if we were looking up the "directed edge" (v --> u), and in
+        that location we store comp(u[..m)). A similar procedure of storing "shadow" edges for
+        quickly looking up forward and backward traversals of a bidirected edge will be taken
+        for the other kinds of edges as well.
+
+                        012*45678*
+        ############  u --------->         beg(u) = 3, end(u) = 9, len(u) = 9
+        # Prefix   #       ||||||
+        # Dovetail #    v <----------      beg(v) = 9, end(v) = 3, len(v) = 10
+        ############       *87654*210
+
+        In this case, a suffix of u aligns to a prefix of comp(v), which is the reverse
+        complement of a suffix of v. In the overlap graph, we represent this with an
+        introverted bidirected edge (u >--< v). We label the (u->v) direction with the
+        sequence comp(v[..end(v)]) and the (v->u) direction with the sequence u[..beg(u)).
+
+        TODO: finish
 
     @var self.n: Number of reads/vertices in graph.
 
-    @var self.g: Adjacency list of graph. For a given u \in V,
-                 if u -> v  is an edge in the graph then self.g[u][v]
-                 exists and is equal to the labelled suffix of the
-                 read overlap u -> v.
+    @var self.g: Adjacency list of graph. self.g[u][v] = (dir(u,v), suflen(u,v))
+
+    #  @var self.g: Adjacency list of graph. For a given u \in V,
+                 #  if u -> v  is an edge in the graph then self.g[u][v]
+                 #  exists and is equal to the
 
     @var self.seqs: Tuple of raw sequencing reads.
 
@@ -26,7 +70,7 @@ class OverlapGraph(object):
         contained = set()
         for u in self.g:
             for v in self.g[u]:
-                if self.g[u][v] == -1:
+                if self.g[u][v][0] == -1:
                     contained.add(v)
 
         pruned = OverlapGraph(self.seqs)
@@ -35,65 +79,9 @@ class OverlapGraph(object):
             if not u in contained:
                 for v in self.g[u]:
                     if not v in contained:
-                        pruned.add_overlap(u, v, self.g[u][v])
+                        pruned.add_overlap(u, v, self.g[u][v][0], self.g[u][v][1])
 
         return pruned
-
-    def get_meyer_tr(self, fuzz):
-
-        VACANT = 0
-        INPLAY = 1
-        ELIMINATED = 2
-
-        n = self.n
-        g = self.g
-        mark = [VACANT for _ in range(n)]
-        reduce = {}
-
-        for v in g:
-            reduce[v] = {}
-            for w in g[v]:
-                reduce[v][w] = False
-
-        neighbors = lambda v: sorted([(w, g[v][w]) for w in g[v]], key=lambda t: t[1])
-
-        for v in range(n):
-            for w in g[v]:
-                mark[w] = INPLAY
-            longest = max([g[v][w] for w in g[v]] + [0]) + fuzz
-
-            v_neighbors = neighbors(v)
-
-            for w, lvw in v_neighbors:
-                if mark[w] == INPLAY:
-                    w_neighbors = neighbors(w)
-                    for x, lwx in w_neighbors:
-                        if lvw + lwx <= longest:
-                            if mark[x] == INPLAY:
-                                mark[x] = ELIMINATED
-
-            for w, lvw in v_neighbors:
-                w_neighbors = neighbors(w)
-                first = True
-                for x, lwx in w_neighbors:
-                    if lwx < fuzz or first:
-                        if mark[x] == INPLAY:
-                            mark[x] = ELIMINATED
-                    first = False
-
-            for w, lvw in v_neighbors:
-                if mark[w] == ELIMINATED:
-                    reduce[v][w] = True
-                mark[w] = VACANT
-
-        tr = OverlapGraph(self.seqs)
-
-        for u in g:
-            for w in g[u]:
-                if not reduce[u][w]:
-                    tr.add_overlap(u, w, g[u][w])
-
-        return tr
 
     def get_naive_tr(self, fuzz):
 
@@ -102,26 +90,66 @@ class OverlapGraph(object):
 
         for u in g:
             reduce[u] = {}
-            for v in g[u]:
-                reduce[u][v] = False
 
         for u in g:
             for v in g[u]:
+                reduce[u][v] = False
+                reduce[v][u] = False
+
+        def edir(u, v): return g[u][v][0]
+        def suflen(u, v): return g[u][v][1]
+        def arrows(u, v): return ((g[u][v][0]>>1)&1, g[u][v][0]&1)
+
+        for u in g:
+            for v in g[u]:
+                uv_head, hv_tail = arrows(u, v)
                 for w in g[v]:
+                    vw_head, vw_tail = arrows(v, w)
                     if w in g[u]:
-                        if g[u][v] + g[v][w] >= g[u][w] + fuzz:
-                            reduce[u][w] = True
+                        uw_head, uw_tail = arrows(u, w)
+                        if uw_tail != vw_head and uv_head == uw_head and vw_tail == uw_tail:
+                            if suflen(u, v) + suflen(v, w) >= suflen(u, w) + fuzz and suflen(v, u) + suflen(w, v) >= suflen(w, u) + fuzz:
+                                reduce[u][w] = True
+                                reduce[w][u] = True
 
         tr = OverlapGraph(self.seqs)
 
         for u in g:
             for w in g[u]:
                 if not reduce[u][w]:
-                    tr.add_overlap(u, w, g[u][w])
+                    assert not reduce[w][u]
+                    tr.add_overlap(u, w, edir(u, w), suflen(u, w))
+                    tr.add_overlap(w, u, edir(w, u), suflen(w, u))
 
         return tr
 
-    def add_overlap(self, u : int, v : int, l : int):
+    #  def get_naive_tr(self, fuzz):
+
+        #  g = self.g
+        #  reduce = {}
+
+        #  for u in g:
+            #  reduce[u] = {}
+            #  for v in g[u]:
+                #  reduce[u][v] = False
+
+        #  for u in g:
+            #  for v in g[u]:
+                #  for w in g[v]:
+                    #  if w in g[u]:
+                        #  if g[u][v] + g[v][w] >= g[u][w] + fuzz:
+                            #  reduce[u][w] = True
+
+        #  tr = OverlapGraph(self.seqs)
+
+        #  for u in g:
+            #  for w in g[u]:
+                #  if not reduce[u][w]:
+                    #  tr.add_overlap(u, w, g[u][w])
+
+        #  return tr
+
+    def add_overlap(self, u : int, v : int, d : int, l : int):
         """
         @instance_method: add_overlap
 
@@ -131,26 +159,20 @@ class OverlapGraph(object):
 
         @param v: The target read index.
 
-        @param l: The length of the sequence that labels this overlap. This is
-                     the suffix of v that overhangs past the last matching
-                     symbol of u and v read from left to right. If v is
-                     contained in u, then l == -1.
+        @param d: The direction going from u to v
+
+        @param l: The overhanging suffix length going from u to v
 
         @raise IndexError: If given invalid read indices.
-
-        @raise ValueError: If the labelled suffix is the empty string.
         """
 
         if u < 0 or u >= self.n or v < 0 or v >= self.n:
             raise IndexError("Trying to add overlap with out of bounds indices")
 
-        if l == 0:
-            raise ValueError("Overlap suffix must not be the empty string")
-
-        if v in self.g[u] and self.g[u][v] <= l:
+        if v in self.g[u] and self.g[u][v][1] <= l:
             return
 
-        self.g[u][v] = l
+        self.g[u][v] = (d, l)
 
     def num_edges(self) -> int:
         """
@@ -174,13 +196,16 @@ class OverlapGraph(object):
         """
         G = IGraph(self.n, directed = True)
         edges = []
-        sufs = []
+        dirs = []
+        suflens = []
         for u, adj in self.g.items():
-            for v, suffix in adj.items():
+            for v, e in adj.items():
                 edges.append((u,v))
-                sufs.append(suffix)
+                dirs.append(e[0])
+                suflens.append(e[1])
         G.add_edges(edges)
-        G.es['suf'] = sufs
+        G.es['dir'] = dirs
+        G.es['len'] = suflens
         G.vs['seq'] = self.seqs
         return G
 
@@ -257,19 +282,35 @@ class OverlapGraph(object):
                     If vpos + vlen <= upos + ulen, then v must be contained inside
                     u.
                     """
-                    g.add_overlap(u, v, -1)
+                    g.add_overlap(u, v, -1, 0)
                 else:
                     """
                     If we reach here, then v extends to the right of u by >= 1 nucleotides.
                     If upos == vpos, then u must be contained within v.
                     """
                     if upos == vpos:
-                        g.add_overlap(v, u, -1)
+                        g.add_overlap(v, u, -1, 0)
                     else:
                         """
                         Reaching here means (upos < vpos) and (upos + ulen < vpos + vlen)
                         """
+
                         suflen = vpos + vlen - upos - ulen
-                        g.add_overlap(u, v, suflen)
+                        prelen = vpos - upos
+
+                        if not urev:
+                            if not vrev:
+                                g.add_overlap(u, v, 1, suflen)
+                                g.add_overlap(v, u, 2, prelen)
+                            else:
+                                g.add_overlap(u, v, 0, suflen)
+                                g.add_overlap(v, u, 0, prelen)
+                        else:
+                            if not vrev:
+                                g.add_overlap(u, v, 3, suflen)
+                                g.add_overlap(v, u, 3, prelen)
+                            else:
+                                g.add_overlap(u, v, 2, suflen)
+                                g.add_overlap(v, u, 1, prelen)
 
         return g
