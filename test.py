78fe617b6620f8d58c4e8f0187a21e8028231e94
Gabriel Raulet <gabe.h.raulet@gmail.com>
2022-06-22 15:12:31 -0700

    added another test script

--- test.py
+++ test.py
@@ -1,28 +1,28 @@
 import AssemblyProblem as AP
 from OverlapGraph import OverlapGraph
+from FindOverlaps import *
 
-genome_length       = 30
-read_depth          = 20
-mean_read_length    = 12
-sd_read_length      = 1
+genome_length       = 100000
+read_depth          = 30
+mean_read_length    = 14050
+sd_read_length      = 1000
 reverse_complements = True
+circular            = True
 
 genome = AP.create_random_genome(genome_length)
-
-seqs, names, records = AP.create_reads(genome, read_depth, mean_read_length, sd_read_length, reverse_complements)
-AP.pretty_layout(seqs, records, "layout.txt")
-AP.pretty_layout(seqs, records)
+seqs, names, records = AP.create_reads(genome, read_depth, mean_read_length, sd_read_length, circular, reverse_complements)
+#  AP.pretty_layout(seqs, records, "layout.txt")
+#  AP.pretty_layout(seqs, records)
 
 overlap_graph = OverlapGraph.generate_gold_standard(seqs, records, genome_length)
-
-overlap_igraph = overlap_graph.get_igraph()
-
-AP.write_fasta("genome.fa", [genome], ["chrom1"])
-AP.write_fasta("reads.fa", seqs, names)
-overlap_igraph.write_gml("overlaps.gml")
-
+AP.write_fasta("AP_100k/genome.fa", [genome], ["chrom1"])
+AP.write_fasta("AP_100k/reads.fa", seqs, names)
+overlap_graph.get_igraph().write_gml("AP_100k/gold_overlap.gml")
 G = overlap_graph.get_pruned()
-H = G.get_naive_tr(0).get_igraph()
-H.delete_vertices(H.vs.select(_degree_eq=0))
-H.write_gml("string.gml")
+G.get_naive_tr(0).get_igraph().write_gml(("AP_100k/gold_string.gml"))
 
+overlap_seeds = get_overlap_seeds(seqs, 31, 19)
+dirty_graph = OverlapGraph.generate_seed_based(seqs, overlap_seeds, 31)
+dirty_graph.get_igraph().write_gml("AP_100k/dirty_overlap.gml")
+H = dirty_graph.get_pruned()
+H.get_naive_tr(0).get_igraph().write_gml("AP_100k/dirty_string.gml")
